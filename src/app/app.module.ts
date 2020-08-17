1d8c1b1f56b56f7494082407d743e1031f95af07
unknown <vinituche@hotmail.com>
2020-08-17 10:34:45 +0100

    Fixed some code detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { MenuNavComponent } from './menu-nav/menu-nav.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MenuNavComponent } from './menu-nav/menu-nav.component';
 
 @NgModule({
   declarations: [

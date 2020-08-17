1d3920a2b325b05dcfd8de73e1c5e8781500a77f
unknown <vinituche@hotmail.com>
2020-08-17 10:03:56 +0100

    ''

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { MenuNavComponent } from './menu-nav/menu-nav.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MenuNavComponent } from './menu-nav/menu-nav.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { MenuNavComponent } from './menu-nav/menu-nav.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu-nav/menu-nav.component.ts
+++ src/app/menu-nav/menu-nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-menu-nav',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-nav.component.css']
 })
 export class MenuNavComponent implements OnInit {
+  @Input() message: string;
+  myFavorite: boolean = false;
+  heartIcon: string = 'favorite_border';
 
-  constructor() { }
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  constructor(private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
+  teste() {
+    console.log(this.message);
+  }
+  
+  toggleFavorite(): void {
+    let message: string;
+
+    this.myFavorite = !this.myFavorite;
+
+    if (this.myFavorite) 
+      message = "Adicionado aos Favoritos";
+    else 
+      message = "Retirado dos Favoritos";
+      
+      this.showMessage(message, 'Fechar');
+  }
+
+  private showMessage(message: string, action: string): void {
+    this._snackBar.open(message, action, {
+      duration: 1000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition
+    });
+  }
 }
